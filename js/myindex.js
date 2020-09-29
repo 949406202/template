98e72ff59648d80080c3ad9e31b6a56bdd6e0cdc
jiangwei <949406202@qq.com>
2020-09-29 17:57:58 +0800

    优化首页界面banner以及导航栏

---
+++ js/myindex.js
@@ -0,0 +1,18 @@
+$(function(){
+	//logo动作 start
+	$("a.logo_a").hover(function(){
+		$(this).css({
+			"font-size":"1.5em",
+			"transition":"0.5s",
+			"-webkit-transition":"0.5s",
+		})
+	},function(){
+		$(this).css({
+			"font-size":"1.7em",
+			"transition":"0.5s",
+			"-webkit-transition":"0.5s",
+		})
+	})
+	//logo动作 end
+	
+})
